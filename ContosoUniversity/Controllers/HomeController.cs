d6c2bbf328c520c0040cd9e8756871da72bdbd3a
wxiong <xiongweihan@gmail.com>
2016-05-30 21:22:37 -0400

    Tutorial finished.

--- ContosoUniversity/Controllers/HomeController.cs
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
 
 using ContosoUniversity.DAL;
 using ContosoUniversity.ViewModels;
+using System.Collections.Generic;
 
 namespace ContosoUniversity.Controllers
 {
@@ -20,13 +18,21 @@ namespace ContosoUniversity.Controllers
 
         public ActionResult About()
         {
-            IQueryable<EnrollmentDateGroup> data = from student in db.Students
-                                                   group student by student.EnrollmentDate into dateGroup
-                                                   select new EnrollmentDateGroup()
-                                                   {
-                                                       EnrollmentDate = dateGroup.Key,
-                                                       StudentCount = dateGroup.Count()
-                                                   };
+            // Commenting out LINQ to show how to do the same thing in SQL.
+            //IQueryable<EnrollmentDateGroup> data = from student in db.Students
+            //                                       group student by student.EnrollmentDate into dateGroup
+            //                                       select new EnrollmentDateGroup()
+            //                                       {
+            //                                           EnrollmentDate = dateGroup.Key,
+            //                                           StudentCount = dateGroup.Count()
+            //                                       };
+
+            // SQL version of the above LINQ code.
+            string query = "SELECT EnrollmentDate, COUNT(*) AS StudentCount "
+                + "FROM school.Person "
+                + "WHERE Discriminator = 'Student' "
+                + "GROUP BY EnrollmentDate";
+            IEnumerable<EnrollmentDateGroup> data = db.Database.SqlQuery<EnrollmentDateGroup>(query);
             return View(data.ToList());
         }
 
